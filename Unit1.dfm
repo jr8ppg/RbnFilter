99efafcd93829a85a234d59398dbcbbdde34d80e
jr8ppg <jr8ppg@gmail.com>
2023-12-04 21:13:01 +0900

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,101 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'RbnFilter'
+  ClientHeight = 299
+  ClientWidth = 619
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = #65325#65331' '#65328#12468#12471#12483#12463
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 12
+  object Label1: TLabel
+    Left = 288
+    Top = 8
+    Width = 291
+    Height = 19
+    Caption = 'RBN RAW'#12487#12540#12479#12398#37325#35079#12434#21066#38500#12375#12414#12377
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = #65325#65331' '#65328#12468#12471#12483#12463
+    Font.Style = []
+    ParentFont = False
+  end
+  object buttonStart: TButton
+    Left = 488
+    Top = 120
+    Width = 113
+    Height = 33
+    Caption = #38283#22987
+    TabOrder = 0
+    OnClick = buttonStartClick
+  end
+  object Memo1: TMemo
+    Left = 8
+    Top = 8
+    Width = 257
+    Height = 281
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = #65325#65331' '#12468#12471#12483#12463
+    Font.Style = []
+    Lines.Strings = (
+      'Memo1')
+    ParentFont = False
+    ScrollBars = ssVertical
+    TabOrder = 1
+  end
+  object editFileName: TEdit
+    Left = 271
+    Top = 72
+    Width = 338
+    Height = 20
+    TabOrder = 2
+  end
+  object buttonFileRef: TButton
+    Left = 520
+    Top = 40
+    Width = 81
+    Height = 26
+    Caption = #21442#29031
+    TabOrder = 3
+    OnClick = buttonFileRefClick
+  end
+  object GroupBox1: TGroupBox
+    Left = 288
+    Top = 128
+    Width = 129
+    Height = 81
+    Caption = 'date'#12398#27604#36611#25991#23383#25968
+    TabOrder = 4
+    object checkDateCompare13: TRadioButton
+      Left = 16
+      Top = 24
+      Width = 113
+      Height = 17
+      Caption = '13'#25991#23383'('#26178#12414#12391')'
+      Checked = True
+      TabOrder = 0
+      TabStop = True
+    end
+    object checkDateCompare16: TRadioButton
+      Left = 16
+      Top = 47
+      Width = 113
+      Height = 17
+      Caption = '16'#25991#23383'('#20998#12414#12391')'
+      TabOrder = 1
+    end
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 456
+    Top = 232
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,163 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Generics.Collections;
+
+type
+  TRbnRecord = class
+    callsign: string;
+    de_pfx: string;
+    de_cont: string;
+    freq: string;
+    band: string;
+    dx: string;
+    dx_pfx: string;
+    dx_cont: string;
+    mode: string;
+    db: string;
+    date: string;
+    speed: string;
+    tx_mode: string;
+  end;
+
+  TRbnList = class(TObjectList<TRbnRecord>)
+  public
+    constructor Create(OwnsObjects: Boolean = True);
+    destructor Destroy(); override;
+  end;
+
+  TForm1 = class(TForm)
+    buttonStart: TButton;
+    Memo1: TMemo;
+    editFileName: TEdit;
+    buttonFileRef: TButton;
+    OpenDialog1: TOpenDialog;
+    GroupBox1: TGroupBox;
+    checkDateCompare13: TRadioButton;
+    checkDateCompare16: TRadioButton;
+    Label1: TLabel;
+    procedure buttonStartClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure buttonFileRefClick(Sender: TObject);
+  private
+    { Private êÈåæ }
+  public
+    { Public êÈåæ }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+   Memo1.Clear();
+end;
+
+procedure TForm1.buttonFileRefClick(Sender: TObject);
+begin
+   if OpenDialog1.Execute(Self.Handle) <> True then begin
+      Exit;
+   end;
+
+   editFileName.Text := OpenDialog1.FileName;
+end;
+
+procedure TForm1.buttonStartClick(Sender: TObject);
+var
+   slFile: TStringList;
+   slLine: TStringList;
+   slFiltered: TStringList;
+   dwTick: DWORD;
+   i: Integer;
+   cnt: Integer;
+   nColumnCount: Integer;
+   list: TDictionary<string, string>;
+   key: string;
+   strFiltered: string;
+   strExt: string;
+   nDateCompare: Integer;
+begin
+   slFile := TStringList.Create();
+   slLine := TStringList.Create();
+   slLine.StrictDelimiter := True;
+   slFiltered := TStringList.Create();
+   list := TDictionary<string, string>.Create();
+   Memo1.Lines.Add('èàóùäJén');
+   try
+      dwTick := GetTickCount();
+
+      if checkDateCompare13.checked = True then begin
+         nDateCompare := 13;
+      end
+      else begin
+         nDateCompare := 16;
+      end;
+
+      strExt := ExtractFileExt(editFileName.Text);
+      strFiltered := ExtractFilePath(editFileName.Text) + ExtractFileName(editFileName.Text);
+      strFiltered := StringReplace(strFiltered, strExt, '', [rfReplaceAll]);
+      strFiltered := strFiltered + '_filtered' + strExt;
+
+      slFile.LoadFromFile(editFileName.Text);
+
+      dwTick := GetTickCount() - dwTick;
+      Memo1.Lines.Add('Loading time = ' + IntToStr(dwTick) + ' ms');
+      Memo1.Lines.Add(IntToStr(slFile.Count) + ' lines');
+
+      nColumnCount := 0;
+      for i := 0 to slFile.Count - 1 do begin
+         slLine.CommaText := Trim(slFile[i]);
+
+         if i = 0 then begin
+            nColumnCount := slLine.Count;
+            slFiltered.Add(slLine.CommaText);
+         end
+         else begin
+            if slLine.Count <> nColumnCount then begin
+               Continue;
+            end;
+
+            // 0        1      2       3    4    5  6      7       8    9  10   11    12
+            // callsign,de_pfx,de_cont,freq,band,dx,dx_pfx,dx_cont,mode,db,date,speed,tx_mode
+
+            // 1234567890123456
+            // 2023-11-25 00:00:00
+            key := slLine[5] + slLine[4] + slLine[12] + Copy(slLine[10], 1, nDateCompare);
+
+            if list.ContainsKey(key) = False then begin
+               list.Add(key, slLine.CommaText);
+               slFiltered.Add(slLine.CommaText);
+               Inc(cnt);
+            end;
+         end;
+      end;
+
+      slFiltered.SaveToFile(strFiltered);
+      Memo1.Lines.Add(IntToStr(cnt) + ' records');
+   finally
+      slFile.Free();
+      slLine.Free();
+      slFiltered.Free();
+      list.Free();
+      Memo1.Lines.Add('èàóùèIóπ');
+   end;
+end;
+
+constructor TRbnList.Create(OwnsObjects: Boolean);
+begin
+   Inherited;
+end;
+
+destructor TRbnList.Destroy();
+begin
+   //
+end;
+
+end.
